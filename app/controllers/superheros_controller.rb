41760c5bf4401c0761e3d4d4cac214b0e41c052d
Justin <justinowenjacot@icloud.com>
2015-06-02 15:42:38 -0400

    added partials and before actions

--- app/controllers/superheros_controller.rb
+++ app/controllers/superheros_controller.rb
@@ -1,5 +1,7 @@
 class SuperherosController < ApplicationController
 
+  before_action :load_superhero, only: [:show, :update, :destroy, :edit]
+
   def index
     @superhero = Superhero.all
   end
@@ -14,26 +16,26 @@ class SuperherosController < ApplicationController
   end
 
   def edit
-    @superhero = Superhero.find(params[:id])
   end
 
   def update
-    @superhero = Superhero.find(params[:id])
     @superhero.update_attributes(superhero_params)
     redirect_to superheros_path
   end
 
   def show
-    @superhero = Superhero.find(params[:id])
   end
 
   def destroy
-    @superhero = Superhero.find(params[:id])
     @superhero.destroy
     redirect_to superheros_path
   end
 
   private
+
+  def load_superhero
+    @superhero = Superhero.find(params[:id])
+  end
 
   def superhero_params
     params.require(:superhero).permit(:name, :powers, :secret_identity, :nemesis, :villains_captured, :image_url, :alliance)
